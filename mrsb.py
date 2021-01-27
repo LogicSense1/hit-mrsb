3cabc658c4279089a04601c175091eed5a474ef0
VinKK1998 <xiawentian1998@outlook.com>
2021-01-27 14:11:09 +0800

    fix geolocation issues

--- mrsb.py
+++ mrsb.py
@@ -17,22 +17,31 @@ else:
 
 if not os.path.exists("mrsb.config"):
     account = input("输入学号：")
-    with open("mrsb.config", 'w') as f:
+    with open("mrsb.config", 'w', encoding='utf-8') as f:
+        context = []
         if keyring.get_password('mrsb', account) == None:
             password = input("输入密码：")
+            context.append(account)
         else:
             password = keyring.get_password('mrsb', account)
-        f.write(account)
+            context.append(account + "\n")
+        location = input("输入地址：")
+        context.append(location)
+        f.writelines(context)
         keyring.set_password('mrsb', account, password)
 else:
-    with open("mrsb.config", 'r') as f:
-        account = f.read()
+    with open("mrsb.config", 'r', encoding='utf-8') as f:
+        context = f.readlines()
+        account = context[0].replace("\n", "")
+        location = context[1].replace("\n", "")
+        print(account)
         password = keyring.get_password('mrsb', account)
 
 mobileEmulation = {'deviceName': 'iPhone X'}
 options = webdriver.ChromeOptions()
 options.add_experimental_option('mobileEmulation', mobileEmulation)
 driver = webdriver.Chrome(executable_path=path, chrome_options=options)
+
 driver.get("https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/shsj/loginChange")
 driver.execute_script("location.replace(\"\/zhxy-xgzs/xg_mobile/shsj/common\")")
 driver.find_element_by_id('mobileUsername').send_keys(account)
@@ -42,9 +51,12 @@ driver.find_element_by_id("mrsb").click()
 driver.execute_script("add()")
 while True:
     try:
+        driver.find_element_by_id("gnxxdz").clear()
+        driver.find_element_by_id("gnxxdz").send_keys(location)
         driver.find_element_by_id("checkbox").click()
         break
     except common.exceptions.NoSuchElementException:
         pass
 driver.execute_script("save()")
+driver.execute_script("document.getElementsByClassName(\"weui-dialog__btn primary\")[0].click()")
 driver.quit()
